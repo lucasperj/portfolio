ac69f98501846803a31279b695bba9fe1ec2752a
Lucas Falcão <lucasmjj09@gmail.com>
2025-07-05 13:44:43 -0300

    feat: Refatorar componente ExpandMore para permitir a passagem de classes e filhos, melhorando a flexibilidade e reutilização do componente

--- src/components/common/ExpandMore.tsx
+++ src/components/common/ExpandMore.tsx
@@ -6,21 +6,30 @@ interface ExpandMoreProps {
   expanded?: boolean;
   onClick?: () => void;
   dataTestId?: string;
+  className?: string;
+  children?: React.ReactNode;
+  [key: string]: any; // Permite outras props como sx, aria-label, etc
 }
 
-// Componente reutilizável de expandir/colapsar, usado em vários lugares
-// Para usar: <ExpandMore dataTestId="nome-unico" />
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expanded, onClick, dataTestId, ...other } = props;
-  return (
-    <IconButton
-      onClick={onClick}
-      className={expanded ? 'expanded' : ''}
-      data-test-id={dataTestId}
-      {...other}
-    />
-  );
-})(({ theme }) => ({
+const ExpandMoreBase: React.FC<ExpandMoreProps> = ({
+  expanded,
+  onClick,
+  dataTestId,
+  className,
+  children,
+  ...other
+}) => (
+  <IconButton
+    onClick={onClick}
+    className={className + (expanded ? ' expanded' : '')}
+    data-test-id={dataTestId}
+    {...other}
+  >
+    {children}
+  </IconButton>
+);
+
+const ExpandMore = styled(ExpandMoreBase)(({ theme }) => ({
   transform: 'rotate(0deg)',
   transition: theme.transitions.create('transform', {
     duration: theme.transitions.duration.shortest,
